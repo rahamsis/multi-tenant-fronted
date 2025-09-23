0ad959cbca4236cd152ebd9d507795064ae398c3
Rahamsis PC <rahamsiscg.95@gmail.com>
2025-09-22 19:05:32 -0500

    fixing importonyperu

--- app/oishipop/[...menu]/page.tsx
+++ app/oishipop/[...menu]/page.tsx
@@ -1,20 +1,26 @@
 
 import { notFound } from "next/navigation";
 
-async function getMenuByUrl(path: string) {
-    // Ejemplo de "menús" en tu DB
-    const menus = [
-        { url: "/productos", titulo: "Productos", contenido: "Aquí están los productos" },
-        { url: "/contacto", titulo: "Contacto", contenido: "Escríbenos un mensaje" },
-    ];
-
-    return menus.find((menu) => menu.url === path) || null;
+interface Props {
+    params: Promise<{ menu: string[] }>;
 }
 
+// async function getMenuByUrl(path: string) {
+//     // Ejemplo de "menús" en tu DB
+//     const menus = [
+//         { url: "/productos", titulo: "Productos", contenido: "Aquí están los productos" },
+//         { url: "/contacto", titulo: "Contacto", contenido: "Escríbenos un mensaje" },
+//     ];
 
-export default async function DynamicPage({ params }: { params: { menu?: string[] } }) {
-    const path = "/" + (params.menu?.join("/") || ""); // "/productos", "/contacto", etc.
-    const menu = await getMenuByUrl(path);
+//     return menus.find((menu) => menu.url === path) || null;
+// }
+
+
+export default async function DinamicPage({ params }: Props) {
+    const { menu } = await params;
+
+    const categoria = menu?.[0];
+    const subCategoria = menu?.[1];
 
     if (!menu) {
         notFound(); // Si no existe en DB, manda 404
@@ -22,8 +28,8 @@ export default async function DynamicPage({ params }: { params: { menu?: string[
 
     return (
         <main className="p-10">
-            <h1 className="text-3xl font-bold">{menu.titulo}</h1>
-            <p className="mt-4">{menu.contenido}</p>
+            <h1 className="text-3xl font-bold">{categoria}</h1>
+            <p className="mt-4">{subCategoria}</p>
         </main>
     );
 }
